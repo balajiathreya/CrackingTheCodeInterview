e29024fb60c47a757d170aae70b9c6c2872143a9
balajiathreya <athreya86@gmail.com>
2015-06-04 17:28:48 -0700

    test git config

--- problems/src/main/java/chapter2/NodeDeletion.java
+++ problems/src/main/java/chapter2/NodeDeletion.java
@@ -6,7 +6,7 @@ import utils.NodeList;
 /**
  * Created by baathreya on 6/3/15.
  *
- * Implement an algorithm to delete a node in the middle of a single linked list, given only access to that node.
+ * Implement an algorithm to delete a node in the middle of a single linked list, GIVEN ONLY ACCESS TO THAT NODE.
  EXAMPLE
  Input: the node ‘c’ from the linked list a->b->c->d->e
  Result: nothing is returned, but the new linked list looks like a->b->d->e
