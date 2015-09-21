1152d31104322b2563e4037bdd1a67294be46114
balajiathreya <athreya86@gmail.com>
2015-09-21 16:43:30 -0700

    linked list from BST

---
+++ problems/src/main/java/chapter4/InorderSuccessor.java
@@ -0,0 +1,18 @@
+package chapter4;
+
+/**
+ * Created by baathreya on 9/15/15.
+ *
+ * Write an algorithm to find the ‘next’ node (i.e., in-order successor) of a given node in a
+ * binary search tree where each node has a link to its parent.
+ *
+ *        x
+ *       / \
+ *      y   z
+ *
+ *      in order: y,x,z
+ *      pre-order: x,y,z
+ *      post-order: y,z,x
+ */
+public class InorderSuccessor {
+}
---
+++ problems/src/main/java/chapter4/LinkedListFromBST.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+/**
+ * Created by baathreya on 9/15/15.
+ *
+ * Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth
+ * (i.e., if you have a tree with depth D, you’ll have D linked lists).
+ *
+ *
+ * extend misc.BreadthFirstSearch.java and modify search() such that it prints all nodes. process() doesn't
+ * have to do anything
+ */
+public class LinkedListFromBST {
+}
